47843dbb2f1c82d3e253970bb409a547148a1e44
Samik <3612424+samik-saha@users.noreply.github.com>
2021-03-15 18:51:28 -0400

    first commit

---
+++ src/main/java/com/galvanize/herobook/HerobookApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.herobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/herobook/HerobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.herobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
