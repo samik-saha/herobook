edf27938ded2e5d2b220a7dac105ed43db58e3e6
sunita1501 <sunita.kumari@cognizant.com>
2021-03-17 19:56:19 -0400

    Red on post villain data

--- src/main/java/com/galvanize/herobook/controller/HeroBookController.java
+++ src/main/java/com/galvanize/herobook/controller/HeroBookController.java
@@ -2,8 +2,10 @@ package com.galvanize.herobook.controller;
 
 import com.galvanize.herobook.model.HeroDTO;
 import com.galvanize.herobook.model.PersonaDTO;
+import com.galvanize.herobook.model.VillainDTO;
 import com.galvanize.herobook.service.HeroesService;
 import com.galvanize.herobook.service.PersonaService;
+import com.galvanize.herobook.service.VillainsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,26 +15,44 @@ import java.util.List;
 public class HeroBookController {
     HeroesService heroesService;
     PersonaService visitorService;
+    VillainsService villainServices;
 
-    public HeroBookController(HeroesService heroesService, PersonaService visitorService){
+
+    public HeroBookController(HeroesService heroesService, PersonaService visitorService,VillainsService villainService){
         this.heroesService=heroesService;
         this.visitorService=visitorService;
+        this.villainServices=villainService;
     }
 
-    @PostMapping("/herobook")
+    @PostMapping("/herobook/hero")
     @ResponseStatus(HttpStatus.CREATED)
     public void postHero(@RequestBody HeroDTO heroDTO){
         heroesService.create(heroDTO);
     }
 
-    @GetMapping("/herobook")
+    @GetMapping("/herobook/hero")
     @ResponseStatus(HttpStatus.OK)
     public List<HeroDTO> getHero() {
 
         return heroesService.fetchAll();
     }
 
-    @PostMapping("/visitor")
+    @PostMapping("/herobook/villain")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postVillain(@RequestBody VillainDTO villainDTO){
+        villainServices.create(villainDTO);
+    }
+
+    @GetMapping("/herobook/villain")
+    @ResponseStatus(HttpStatus.OK)
+    public List<VillainDTO> getVillain() {
+
+        return villainServices.fetchAll();
+    }
+
+
+
+    @PostMapping("/herobook/visitor")
     @ResponseStatus(HttpStatus.CREATED)
     public void postHero(@RequestBody PersonaDTO visitorDTO){
         visitorService.create(visitorDTO);
