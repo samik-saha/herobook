da4ab0a18f3a77c96dbe875529276078ec781e7e
sunita1501 <sunita.kumari@cognizant.com>
2021-03-18 18:39:14 -0400

    Green on post villain data

--- src/main/java/com/galvanize/herobook/model/PersonaEntity.java
+++ src/main/java/com/galvanize/herobook/model/PersonaEntity.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 @EqualsAndHashCode
 @Data
 @NoArgsConstructor
--- src/main/java/com/galvanize/herobook/service/PersonaRepository.java
+++ src/main/java/com/galvanize/herobook/service/PersonaRepository.java
@@ -2,6 +2,8 @@ package com.galvanize.herobook.service;
 
 import com.galvanize.herobook.model.PersonaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonaRepository extends JpaRepository<PersonaEntity, Long> {
 }
